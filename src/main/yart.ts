6d2cf5c2f1f5515869c6238a0078a0babeae786f
Nikolaos Georgiou <nikolaos.georgiou@gmail.com>
2021-10-29 03:58:50 +0200

    Removed ts code

--- src/main/yart.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { main } from './main';
-
-main().catch(err => {
-  console.log(err.message);
-  process.exitCode = 1;
-});
--- src/test/utils/path-utils.ts
+++
@@ -1,5 +0,0 @@
-import path = require('path');
-
-export function fixturePath(relativePath: string): string {
-  return path.join(__dirname, '..', 'fixtures', relativePath);
-}
